f87c41d7ec2e65708cf5088c8a993631d433c608
reggykoketso <168643546+reggykoketso@users.noreply.github.com>
2024-09-04 18:49:28 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react-native';
+import {  View,image, Text} from 'react-native';
+
+export default function App() {
+  <View style={{backgroundColor:"red",justifyContent:"space-evenly",flexDirection:"column",alignItems:"center"}}>
+  <View style={style.container}>
+  c
+
+  </View>
+   
+
+   </View>
+
+   
+   
+
+}
+
+
